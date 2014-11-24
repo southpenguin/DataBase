ea9be2b899185ccde1c96c41cc761493ca4ac7ae
rean.wen <reaniwen@gmail.com>
2014-11-23 23:20:09 -0500

    v3.1
    make some change based on v3.0, also made change on art

--- scheme.sql
+++ scheme.sql
@@ -13,8 +13,7 @@ Drop table `project`.User;
 Drop table `project`.Content;
 
 create table User (
-	uid int(10),
-	uusername varchar(20),
+	uid varchar(20),
 	upassword varchar(20),
 	ufname varchar(20),
 	ulname varchar(20),
@@ -25,10 +24,9 @@ create table User (
 );
 
 create table Art (
-	aid int(10),
+	aid varchar(20),
 	apassword varchar(20),
-	afname varchar(20),
-	alname varchar(20),
+	artname varchar(20),
 	aemail varchar(40),
 	asite varchar(100),
 	primary key (aid)
@@ -41,15 +39,15 @@ create table Genre (
 );
 
 create table Follow (
-	followee int(10),
-	follower int(10),
+	followee varchar(20),
+	follower varchar(20),
 	foreign key (follower) references User(uid),
 	foreign key (followee) references User(uid)
 );
 
 create table Fans (
-	fan int(10),
-	follow int(10),
+	fan varchar(20),
+	follow varchar(20),
 	foreign key (fan) references User(uid),
 	foreign key (follow) references Art(aid)
 );
@@ -58,9 +56,9 @@ create table Location(
 	lid int(10),
 	lname char(20),
 	lnumber int(8),
-	street1 char(40),
-	street2 char(40),
-	city char(30),
+	street1 varchar(40),
+	street2 varchar(40),
+	city varchar(30),
 	state char(2),
 	zip int(5),
 	primary key (lid)
@@ -69,7 +67,7 @@ create table Location(
 
 create table Concert (
 	cid int(10),
-	cname char(40),
+	cname varchar(40),
 	date int(8),
 	time int(4),
 	price decimal(7,2),
@@ -83,31 +81,31 @@ create table Concert (
 );
 
 create table Likes (
-	luid int(10),
+	luid varchar(20),
 	lgenre int(2),
 	foreign key (luid) references User(uid),
 	foreign key (lgenre) references Genre(gid)
 );
 
 create table Have (
-	haid int(10),
+	haid varchar(20),
 	hgenre int(2),
 	foreign key (haid) references Art(aid),
 	foreign key (hgenre) references Genre(gid)	
 );
 
 create table Attend (
-	auid int(10),
+	auid varchar(20),
 	acid int(10),
 	rate int(1),
-	review text(500),
+	review varchar(500),
 	foreign key (auid) references User(uid),
 	foreign key (acid) references Concert(cid)
 );
 
 create table Lists (
 	listid int(10),
-	luid int(10),
+	luid varchar(20),
 	primary key (luid, listid),
 	foreign key (luid) references User(uid)
 );
@@ -121,7 +119,7 @@ create table Content (
 
 create table FollowList (
 	flistid int(10),
-	fluid int(10),
+	fluid varchar(20),
 	foreign key (flistid) references Lists(listid),
 	foreign key (fluid) references User(uid)
 );
